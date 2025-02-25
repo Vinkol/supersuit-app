8b17d4b089b109354d5fbdbb87f26be8c45052df
Dmitriy Yashin <vinkoldima@gmail.com>
2025-02-25 22:05:41 +0300

    add motion

--- src/components/CostumePreview/CostumePreview.jsx
+++ src/components/CostumePreview/CostumePreview.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./CostumePreview.module.sass";
+import { motion } from "framer-motion";
 
 const CostumePreview = (props) => {
     const { id, image, name, size, price, stock } = props;
   
     return (
-      <div className={styles.card}>
-        <img className={styles.image} src={image} alt={name} />
-        <h4 className={styles.name}>{name}</h4>
-        <p className={styles.size}>Размер: {size}</p>
-        <h5 className={styles.price}>{price}₽/день</h5>
-        <Link to={`/costume/${id}`} className={styles.quickView}>
-          Быстрый просмотр
-        </Link>
-        <p className={stock > 0 ? styles.stock : `${styles.stock} ${styles.outOfStock}`}>
-            {stock > 0 ? `В наличии: ${stock}` : "Нет в наличии"}
-        </p>
-      </div>
+      <motion.div
+        className={styles.card}
+        whileHover={{ scale: 0.98 }}
+        transition={{ duration: 0.3 }}
+        >
+        <div className={styles.card}>
+          <img className={styles.image} src={image} alt={name} />
+          <h4 className={styles.name}>{name}</h4>
+          <p className={styles.size}>Размер: {size}</p>
+          <h5 className={styles.price}>{price}₽/день</h5>
+          <Link to={`/costume/${id}`} className={styles.quickView}>
+            Быстрый просмотр
+          </Link>
+          <p className={stock > 0 ? styles.stock : `${styles.stock} ${styles.outOfStock}`}>
+              {stock > 0 ? `В наличии: ${stock}` : "Нет в наличии"}
+          </p>
+        </div>
+      </motion.div>
     );
   };
   
